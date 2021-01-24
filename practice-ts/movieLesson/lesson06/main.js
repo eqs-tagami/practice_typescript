3090daddd59ceec1f55a297b54458dc5c8e71d0f
田上翔斗 <shoto@stagami.local>
2021-01-25 00:22:55 +0900

    アロー関数

---
+++ practice-ts/movieLesson/lesson06/main.js
@@ -0,0 +1,8 @@
+// 関数
+/*
+let add = (a: number, b: number): number => {
+  return a + b;
+}
+*/
+var add = function (a, b) { return a + b; };
+console.log(add(5, 3));
---
+++ practice-ts/movieLesson/lesson06/main.ts
@@ -0,0 +1,10 @@
+// 関数
+/*
+let add = (a: number, b: number): number => {
+  return a + b;
+}
+*/
+
+let add = (a: number, b: number): number => a + b;
+
+console.log(add(5, 3));
