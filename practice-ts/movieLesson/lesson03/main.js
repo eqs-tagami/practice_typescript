aa422975e014e3482d4997a97ee4f223df51ecaf
田上翔斗 <shoto@stagami.local>
2021-01-24 23:58:26 +0900

    型の種類について

---
+++ practice-ts/movieLesson/lesson03/main.js
@@ -0,0 +1,13 @@
+// 型
+/*
+number
+string
+boolean
+any
+*/
+var i;
+var ii = 10;
+var iii = 10;
+var x; //var x: any
+var results;
+results = [10, 5, 3];
---
+++ practice-ts/movieLesson/lesson03/main.ts
@@ -0,0 +1,17 @@
+// 型
+
+/*
+number
+string
+boolean
+any
+*/
+
+var i: number;
+var ii: number = 10;
+var iii = 10;
+
+var x; //var x: any
+
+var results: number[];
+results = [10, 5, 3];
